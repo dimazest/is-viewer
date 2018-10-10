4cd07b16dc45065f8114b42d744fb1dc4168b991
Dmitrijs Milajevs <dimazest@gmail.com>
2018-10-10 16:51:10 -0400

    Updates

---
+++ precache-manifest.1263817afc3856244ed326690f4ca36d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/is-viewer/static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "aa5861cfc2aac1afe8f9",
+    "url": "/is-viewer/static/css/main.ebf14fec.chunk.css"
+  },
+  {
+    "revision": "eca2c26759a009a4a9361151efe99e74",
+    "url": "/is-viewer/static/media/open-iconic.eca2c267.svg"
+  },
+  {
+    "revision": "28205f3ea0e678f50ce8",
+    "url": "/is-viewer/static/js/1.28205f3e.chunk.js"
+  },
+  {
+    "revision": "4dfd2eb58fc4c1612603",
+    "url": "/is-viewer/static/js/runtime~main.4dfd2eb5.js"
+  },
+  {
+    "revision": "aa5861cfc2aac1afe8f9",
+    "url": "/is-viewer/static/js/main.aa5861cf.chunk.js"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "/is-viewer/static/media/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/is-viewer/static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/is-viewer/static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "28205f3ea0e678f50ce8",
+    "url": "/is-viewer/static/css/1.ef270576.chunk.css"
+  },
+  {
+    "revision": "6ce350096dd4bde489ca0c4bd8e1ec64",
+    "url": "/is-viewer/index.html"
+  }
+];
